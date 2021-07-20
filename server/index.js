37f9765a493cc37c8e58558e525fa940a88ed04b
GiaKhang <waoknight@gmail.com>
2021-07-20 16:17:56 +0700

    send static file

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const PORT = process.env.PORT || 3000
 
+app.use(express.static(path.join(__dirname + '/../client')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, resp) => {
-	resp.send('SUCCESS')
+	resp.sendFile(path.join((__dirname + '/../client/src/main.html')))
+	//console.log(__dirname)
+	//resp.send(__dirname)
+	//resp.sendFile('/src/main.html')
 })
 
 app.listen(PORT, () => {
-	console.log(`Server is listen on ${ PORT }`)
+	console.log(`Server is listen on PORT ${ PORT }`)
 })
